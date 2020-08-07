6e8cd1cb12fbd7506582dba8f64dcdff915200bb
cerruttipablo <pablodcc12@gmail.com>
2020-08-07 19:51:19 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -18,7 +18,7 @@ var hideSpinner = function(){
 var getJSONData = function(url){
     var result = {};
     showSpinner();
-    return fetch(url)
+    return fetch(CATEGORIES_URL)
     .then(response => {
       if (response.ok) {
         return response.json();
