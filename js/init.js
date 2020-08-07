112abc84fcd15a4b00a95e3a7073eb9ebc6f8bab
cerruttipablo <68930838+cerruttipablo@users.noreply.github.com>
2020-08-07 18:54:51 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -15,7 +15,7 @@ var hideSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "none";
 }
 
-var getJSONData = function(url){
+var getJSONData = function(CATEGORIES_URL){
     var result = {};
     showSpinner();
     return fetch(url)
